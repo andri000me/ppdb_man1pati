82d2cdc61eef32e33547252c3a72b7a344e8e979
HR77scu <55226658+HR77scu@users.noreply.github.com>
2021-01-22 17:24:33 +0700

    Update config.php
    ini

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 // $config['base_url'] = 'http://ppdb.smkwikrama1jepara.sch.id/';
-$config['base_url'] = 'http://localhost/ppdb_MAN/';
+$config['base_url'] = 'http://localhost/ppdb_man1pati/';
 
 /*
 |--------------------------------------------------------------------------
