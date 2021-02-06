b0c21e323132a83e711a9a722f06b1365b944d9c
HR77scu <rafiasik77@gmail.com>
2021-02-07 01:02:19 +0700

    07-febuary-2021|1:02

--- application/views/percobaan1.php
+++ application/views/percobaan1.php
@@ -1,55 +1,198 @@
 <!DOCTYPE html>
 <html>
-
 <head>
-  <!-- Metas -->
-  <meta charset="utf-8">
-  <title>Foxeresto - Restaurant Template</title>
-  <meta name="description" content="">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Css -->
+	<title>Cetak Kartu</title>
+	<style type="text/css">
+		body{
+			background-color: whitesmoke;
+		}	
+		.card{
+			width: 60%;
+			height: 380px;
+			background-color: white;
+			border: 1px solid black
+		}
+
+		.col-img{
+			width: 20%;
+			background-color: green;
+			height: 100px
+			/*padding: 10px; */
+		}
+		.icon-img{
+			width: 100px;
+			margin-left: 20%;
+		}
+		.col-header{
+			background-color: green;
+			height: 100px;
+			width: 80%;
+			/*margin-left: 15px;*/
+		}
+		.col-data{
+			width: 50%;
+			height: 150px;
+			/*background-color: yellow;*/
+		}
+		.col-profil{
+			width: 50%;
+			height: 150px;
+			/*margin-left: 10%;*/
+			/*background-color: blue;*/
+		}
+		.view-data{
+			font-size: 15px;
+			/*margin-left: 100px;*/
+		}
+		.img-profil{
+			width: 100px; 
+			margin-left: 30%; 
+			height: 150px;
+		}
+		.col-1{
+			font-size: 13px;
+			margin-left: 10px; 
+			margin-top: 10px
+		}
+					.hr1{
+				border: 1px solid black;
+			}
+			.hr2{
+				border: 1px solid white
+			}
+
+		/*@media screen and (max-height: )*/
+
+		@media only screen and (max-width: 780px)
+		{
+			.card{
+				width: 100%;
+				height: 380px;
+				background-color: white;
+			}
+			.col-img{
+				height: 60px;
+				background-color: green;
+				width: 20%;
+			}
+			.icon-img{
+				width: 80%;
+				margin-left: 10%;
+				/*height: 50px;*/
+			}
+			.col-header{
+				background-color: green;
+				height: 60px;
+				width: 80%
+			}
+			/*width: 80px; margin-left: 20%;*/
+			h3{
+				font-size: 60%;
+			}
+			p{
+				font-size: 60%;
+			}
+			.col-data{
+				width: 60%;
+				height: 100px;
+			}
+			.col-profil{
+				width: 40%;
+				height: 100px;
+			}
+			.view-data{
+				font-size: 10px;
+			}
+			.img-profil{
+				width: 50px;
+				margin-left: 30%; 
+				height: 100px;
+			}
+			.col-1{
+				font-size: 13px;
+				margin-left: 10px; 
+				margin-top: 10px
+			}
+			.hr1{
+				border: 1px solid black;
+			}
+			.hr2{
+				border: 1px solid white
+			}
+
+		}
+	</style>
 </head>
+<body>
+	<div class="card">
+		<div class="row header">
+			<div class="col-img" style="float: left;">
+				<img src="https://ppdb.man2pati.sch.id/assets/img/logo1.png" class="icon-img">
+			</div>
+			<div class="col-header" style="float: left; ">
+				<center>
+					<h3>KARTU PENDAFTARAN PESERTA DIDIK BARU KELAS</h2>
+					<p>UNGGULAN MAN 2 PATI TAHUN 2021/2022</p>
+				</center>
+			</div>
 
-<body class="bg-primary" onload="window.print()">
+		</div>
+		<hr class="hr1" >
+		<div class="row">
+			<div class="col-data" style="float: left;" >
+				<div class="view-data" style="margin-left: 10px;">
+				<table>
+					<tr>
+						<td>ID Pendaftaran</td>
+						<td>:</td>
+						<td><?= $isi['id_pendaftaran'] ?></td>
+					</tr>
+					<tr>
+						<td>Nama Lengkap</td>
+						<td>:</td>
+						<td><?= $isi['nm_lengkap'] ?></td>
+					</tr>
+										<tr>
+						<td>Jenis Kelamin</td>
+						<td>:</td>
+						<td><?= $isi['jns_kelamin'] ?></td>
+					</tr>
+										<tr>
+						<td>Tanggal Lahir</td>
+						<td>:</td>
+						<td><?= $isi['tgl_lahir'] ?></td>
+					</tr>
+										<tr>
+						<td>Tanggal Pendaftaran</td>
+						<td>:</td>
+						<td><?= date('d-m-y') ?></td>
+					</tr>					<tr>
+						<td>Tangal Seleksi/Test</td>
+						<td>:</td>
+						<td>1 Maret 2021</td>
+					</tr>
+										<tr>
+						<td>Tempat Seleksi</td>
+						<td>:</td>
+						<td>MAN 2 PATI</td>
+					</tr>
 
-  <div class="container" style="width:100%; margin-top:10%;">
-    <div class="row card">
-      <nav class="navbar navbar-width bg-whit">
-        <div class="col-md-3">
-        </div>
-        <div class="col-md-9">
-          <p style="margin-left:30%; margin-top:-5%;">KARTU PENDAFTARAN PESERTA DIDIK BARU KELAS </p> <br>
-          <img src="<?php echo base_url(); ?>assets/img/logo1.png" style="margin-left:10%; margin-top:-30%;">
-          <p style="margin-top:-2%; margin-left:33%;">UNGGULAN MAN 2 PATI TAHUN 2021/2022</p> <br>
+				</table>
+				</div>
+			</div>
+			<div class="col-profil" style="float: left; ">
+				<img src="<?= base_url(); ?>/upload/rapot/<?= $isi['nm_file_foto'] ?>" class="img-profil" >
+			</div>
+		</div>
+		<hr class="hr2">
+		<div class="row" >
+			<div class="col-1"  >
+				<p >Selamat anda telah mendaftar sebagai calon peserta didik MAN2 pati jalur kelas unggulan tahun pelajaran 2021/2022.</p>
+				<p>NB. Untuk info lebih lanjut bisa hubungi Pak Sunaryo : 081325651645 atau Bu Rizka : 0813867129696 </p>
+			</div>
 
-        </div>
-      </nav>
-      <hr>
-      <div class="card-group">
-        <div class="row">
-          <div class="col-md-6">
-            <p class="col">ID Pendaftaran &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <small class="text-muted"> : <?= $isi['id_pendaftaran'] ?> </p>
-            <p class="col" style="margin-top:-1%;">Nama Lengkap &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<small class="text-muted"> : <?= $isi['nm_lengkap'] ?></small></p>
-            <img src="<?= base_url(); ?>/upload/rapot/<?= $isi['nm_file_foto'] ?>" style="width:10%; margin-left:70%; margin-top:-80%;">
-            <p class="col" style="margin-top:-2%;">Jenis Kelamin &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<small class="text-muted"> : <?= $isi['jns_kelamin'] ?> </small></p>
-            <p class="col" style="margin-top:1%;"><small class="text-muted">Tanggal Lahir &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<small class="text-muted"> : <?= $isi['tgl_lahir'] ?> </small></p>
-            <p class="col" style="margin-top:1%;">Tanggal Pendaftaran &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<small class="text-muted"> : <?= date('d-m-y') ?> </small></p>
-            <p class="col" style="margin-top:1%;">Tanggal seleksi/test &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <small class="text-muted"> : 1 maret 2021</small></p>
-            <p class="col" style="margin-top:1%;"> Tempat &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp <small class="text-muted"> : MAN 2 Pati</small></p>
+		</div>
 
-          </div>
-          <div class="col-md-6">
-          </div>
-          <div class="col-md-12">
-            <p> <small class="text-muted" style="margin-left:20%;">Selamat anda telah mendaftar sebagai calon peserta didik MAN2 pati jalur kelas unggulan tahun pelajaran 2021/2022.</small></p>
-          </div>
-          <div class="col-md-12">
-            <p> <small class="text-muted" style="margin-left:23%;">NB. Untuk info lebih lanjut bisa hubungi Pak Sunaryo : 081325651645 atau Bu Rizka : 0813867129696</small></p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	</div>
 </body>
-
 </html>
